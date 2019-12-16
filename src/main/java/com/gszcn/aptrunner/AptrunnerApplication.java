e45107218b308625d3fa34c1ec68052644f836b1
leiyunlong <1175333149@qq.com>
2019-12-16 10:51:28 +0800

    Initial commit

---
+++ src/main/java/com/gszcn/aptrunner/AptrunnerApplication.java
@@ -0,0 +1,13 @@
+package com.gszcn.aptrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptrunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptrunnerApplication.class, args);
+    }
+
+}
